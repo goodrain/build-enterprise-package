4e42fea93a8679d6c8eff0da6a6b9d8808bc5ea0
yangkaa <30614084+yangkaa@users.noreply.github.com>
2021-10-27 18:22:37 +0800

    Update enterprise_offline_package.sh

--- enterprise_offline_package.sh
+++ enterprise_offline_package.sh
@@ -2,7 +2,7 @@
 
 export NFSCLI_URL="https://rainbond-pkg.oss-cn-shanghai.aliyuncs.com/offline/nfs-client/nfs_all.tar.gz"
 export DOCKER_VER=19.03.5
-RBD_VER=${RBD_VER:-'enterprise-2108'}
+RBD_VER=${RBD_VER:-'enterprise-2110'}
 
 function get_nfscli() {
 
